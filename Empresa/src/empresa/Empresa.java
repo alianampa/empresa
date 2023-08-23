010e5e3df199acc5376357dbc0e1a99ce35f3ce4
aanampa08 <aliianampa2003@gmail.com>
2023-08-23 19:22:01 -0300

    Codigo corregido

--- Empresa/src/empresa/Empresa.java
+++ Empresa/src/empresa/Empresa.java
@@ -4,11 +4,9 @@ public class Empresa {
 
     public static void main(String[] args) {
         for(int i = 0; i < 100 ; i=i+2){
-            System.out.println("numero: " + i);
-            
-            CAMBIAR EL CODIGO, USAR CONDICIONALES
-            
-            
+            if (i % 2 == 0){
+                System.out.println("Numero: "+i);
+            }
         }
            
     }
